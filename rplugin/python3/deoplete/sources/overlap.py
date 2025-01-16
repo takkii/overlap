b6d4fc851695cd89ce8161dbaa0e535501d4d3c6
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-01-17 07:53:09 +0900

    Update.

--- rplugin/python3/deoplete/sources/overlap.py
+++ rplugin/python3/deoplete/sources/overlap.py
@@ -23,7 +23,7 @@ class Source(Base):
         super().__init__(vim)
         self.name: Optional[str] = 'overlap'
         self.filetypes: Optional[list] = ['php']
-        mark_synbol: Optional[str] = '[php_method_pandas]'
+        mark_synbol: Optional[str] = '[PHP_pandas]'
         self.mark: Optional[str]  = str(mark_synbol)
         ruby_match: Optional[list] = [r'\.[a-zA-Z0-9_?!]*|[a-zA-Z]\w*::\w*']
         slash_none: Optional[list] = [r'[;/[^¥/]\*/]']
