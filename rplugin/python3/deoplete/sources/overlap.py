f9bd0a545317a53ec38afee7ab27b5c6ee9c00af
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-07-27 09:26:39 +0900

    Update.

--- rplugin/python3/deoplete/sources/overlap.py
+++ rplugin/python3/deoplete/sources/overlap.py
@@ -21,12 +21,12 @@ class Source(Base):
     def __init__(self, vim):
         super().__init__(vim)
         self.name: Optional[str] = 'overlap'
-        self.filetypes: Optional[list] = ['php', 'html']
+        self.filetypes: Optional[list] = ['php']
         mark_synbol: Optional[str] = '[pandas: ' + str(pd.__version__) + ']'
         self.mark: Optional[str] = str(mark_synbol)
-        php_match: Optional[list] = [r'\.[a-zA-Z0-9_?!]*|[a-zA-Z]\w*::\w*']
+        ruby_match: Optional[list] = [r'\.[a-zA-Z0-9_?!]*|[a-zA-Z]\w*::\w*']
         html_match: Optional[list] = [r'[<a-zA-Z(?: .+?)?>.*?<\/a-zA-Z>]']
-        self.input_pattern: Optional[str] = '|'.join(php_match + html_match)
+        self.input_pattern: Optional[str] = '|'.join(ruby_match + html_match)
         self.rank: Optional[int] = 500
 
     def get_complete_position(self, context):
@@ -42,55 +42,35 @@ class Source(Base):
             # 3.5 and higher, 4.x or less,python version is required.
             if (py_mj == 3 and py_mi > 4) or (py_mj < 4):
 
-                # Settings, vim-plug | vim path is true/false folder search.
-                vim_f: Optional[str] = '~/.vim/plugged/dict/load/php/'
-                vim_t = '~/.vim/plugged/dict/load/php/php_dict_read.txt'
-
                 # Settings, $HOME/dict path is true/false folder search.
-                loc_f: Optional[str] = '~/dict/'
-                loc_t: Optional[str] = '~/dict/php_dict_read.txt'
+                loc_t: Optional[str] = 'load/php/'
 
-                # Home Folder, Set the dictionary.
-                if os.path.exists(os.path.expanduser(loc_f)):
+                paths = [
+                    os.path.expanduser(os.path.join(p, loc_t)) for p in [
+                        '~/GitHub/dict/', '~/.vim/plugged/dict/',
+                        '~/.neovim/plugged/dict/'
+                    ]
+                ]
 
-                    # Get Receiver/overlap behavior.
-                    with open(os.path.expanduser(loc_t)) as r_meth:
-                        # pandas and dask
-                        index_php: Optional[list] = list(r_meth.readlines())
-                        pd_php = pd.Series(index_php)
-                        st_r = pd_php.sort_index()
-                        ddf = from_pandas(
-                            data=st_r, npartitions=multiprocessing.cpu_count())
-                        data_array = ddf.to_dask_array(lengths=True)
-                        data = data_array.compute()
-                        data_py: Optional[list] = [s.rstrip() for s in data]
+                path = next(p for p in paths if os.path.exists(p))
+                php_dict: Optional[str] = 'php_dict_read.txt'
+                php_mod_fn = os.path.join(path, php_dict)
 
-                        # sorted and itemgetter
-                        sorted(data_py, key=itemgetter(0))
-                        return data_py
+                # Get Receiver/overlap behavior.
+                with open(php_mod_fn) as r_meth:
+                    # pandas and dask
+                    index_ruby: Optional[list] = list(r_meth.readlines())
+                    pd_ruby = pd.Series(index_ruby)
+                    st_r = pd_ruby.sort_index()
+                    ddf = from_pandas(data=st_r,
+                                      npartitions=multiprocessing.cpu_count())
+                    data_array = ddf.to_dask_array(lengths=True)
+                    data = data_array.compute()
+                    data_py: Optional[list] = [s.rstrip() for s in data]
 
-                # Vim Folder, Set the dictionary.
-                elif os.path.exists(os.path.expanduser(vim_f)):
-
-                    # Get Receiver/overlap behavior.
-                    with open(os.path.expanduser(vim_t)) as r_meth:
-                        # pandas and dask
-                        vim_php: Optional[list] = list(r_meth.readlines())
-                        pd_php = pd.Series(vim_php)
-                        st_r = pd_php.sort_index()
-                        ddf = from_pandas(
-                            data=st_r, npartitions=multiprocessing.cpu_count())
-                        data_array = ddf.to_dask_array(lengths=True)
-                        data = data_array.compute()
-                        vim_py: Optional[list] = [s.rstrip() for s in data]
-
-                        # sort and itemgetter
-                        vim_py.sort(key=itemgetter(0))
-                        return vim_py
-
-                # Config Folder not found.
-                else:
-                    raise ValueError("None, Please Check the Config Folder")
+                    # sorted and itemgetter
+                    sorted(data_py, key=itemgetter(0))
+                    return data_py
 
             # Python_VERSION: 3.5 or higher and 4.x or less.
             else:
@@ -98,9 +78,15 @@ class Source(Base):
 
         # TraceBack.
         except Exception:
+            # Check, overlap.py path.
+            git_o = File.basename(
+                File.expand_path(
+                    "~/~/.vim/plugged/overlap/rplugin/python3/deoplete/sources/overlap.py"
+                ), ".py") + "_log"
+
             # Load/Create LogFile.
-            except_folder: Optional[str] = '~/overlap_log/'
-            except_file: Optional[str] = '~/overlap_log/overlap_error.log'
+            except_folder: Optional[str] = '~/' + git_o
+            except_file: Optional[str] = '~/' + git_o + '/error.log'
 
             # Load the dictionary.
             if os.path.isdir(os.path.expanduser(except_folder)):
